8ec11628bcff86bb223c2f3122dca000de51d0da
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-IH8VEDN>
2021-01-26 11:01:18 -0300

    alterações de imagens para minha própria imagem(alem de... )

--- painel/paginas/includes/menus.php
+++ painel/paginas/includes/menus.php
@@ -1,21 +1,16 @@
 
-  <!-- Preloader -->
-  <div class="preloader">
-    <img src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
-  </div>
-
   <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light" >
     <!-- Left navbar links -->
     <ul class="navbar-nav">
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
+        <a href="?pg=dashboard" class="nav-link">Inicio</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
+        <a href="#" class="nav-link">Contato</a>
       </li>
     </ul>
 
@@ -37,8 +32,8 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <a href="?pg=inicio" class="brand-link">
+      <img src="painel/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
@@ -47,10 +42,10 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="painel/dist/img/vitinhopolemico.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="https://www.instagram.com/vitinho_tavares_013/?hl=pt-br" class="d-block">Victor Tavares</a>
         </div>
       </div>
 
@@ -63,10 +58,26 @@
                with font-awesome or any other icon font library -->
           <li class="nav-item menu-open">            
           <li class="nav-item">
-            <a href="pages/widgets.html" class="nav-link">
+              <a href="?pg=produtos" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
               <p>
                 Produtos                
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+              <a href="?pg=serviços" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Serviços                
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+              <a href="?pg=Contato" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Contato               
               </p>
             </a>
           </li>
